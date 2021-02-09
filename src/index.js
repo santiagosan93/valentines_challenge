3025ccdd9479baf672b5700d71d3520703c9424a
santiago sanchez <soysantiagosanchezo@gmail.com>
2021-02-09 15:25:47 +0100

    sets pseudo code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello from src/index.js!");
+// Steps!
+
+// 1. bind an event listener on the form when it gets submited --> (.addEventListener)
+// 2. Check the sugested link and understand the type 'submit'
+
+// Inside the listener
+//---------------------
+// 3. Select all the inputs that have the data-set of data correct-anser to true
+// Hint 👆 document.querySelectorAll("[data-foo='1']")
+
+// 4. set a counter to 0
+
+// 5. Iterate over the inputs previously selected and increment the counter by 10 for each input that is selected
+// Hint ☝️ #inputElement.checked# returns a boolean
+
+// 6.1 select your score div of your HTML
+// 6.2 take your score (the counter) an insert the value in the div with the class of score in your html file
+
+// 7. Go crazy on the style of the score div!
+
+
+
+
