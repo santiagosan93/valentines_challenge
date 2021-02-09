a4f50b512b7aecb8e16220ff6457f434c6cd2aa5
santiago sanchez <soysantiagosanchezo@gmail.com>
2021-02-09 15:39:02 +0100

    pseudo re-write

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 // Steps!
-
-// 1. bind an event listener on the form when it gets submited --> (.addEventListener)
-// 2. Check the sugested link and understand the type 'submit'
-
+// 1. Select the whole form element
+// 2. Add an event listener on the form for when it gets submitted (see the link)
 // Inside the listener --> Prevent the default behaviour of the event.
 //---------------------
-// 3. Select all the inputs that have the data-set of data correct-anser to true
-// Hint 👆 document.querySelectorAll("[data-foo='1']")
-
-// 4. set a counter to 0
-
-// 5. Iterate over the inputs previously selected and increment the counter by 10 for each input that is selected
+// 3. Select all the inputs that have the data-set of data correct-answer to true
+// Hint 👆 more advanced css selectors: ("[data-foo='1']")
+// 4. Find a way to compute the score (e.g. set a counter?)
+// 5. Iterate over the inputs previously selected and add the score for each input that is checked
 // Hint ☝️ #inputElement.checked# returns a boolean
-
-// 6.1 select your score div of your HTML
-// 6.2 take your score (the counter) an insert the value in the div with the class of score in your html file
-
+// 6.1 Select your score div of your HTML
+// 6.2 Take your score (the counter) an insert the value in the div with the class of score in your html file
 // 7. Go crazy on the style of the score div!
 
 
